5eb6efa13ec038834d8dddd1712cca317d13ce1f
unknown <madhunaraaaaa@gmail.com>
2025-04-10 10:03:49 +0700

    Komputasi_Matematika

---
+++ Praktikum_MatLab_2.m
@@ -0,0 +1,17 @@
+clc
+clear %untuk menghapus 
+%untuk perintah tanpa menjalankan (comment)
+
+% Menghitung luas persegi panjang 
+p = 10;
+l= 20;
+lp= p * l ; 
+disp (lp)
+
+lp1 = p + l;
+lp2 = p-l;
+lp3 = p/l;
+
+disp(lp1)
+disp(lp2)
+disp(lp3)
